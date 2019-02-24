e8d972f7834e3943adb813b0dbd7eb80e09142b9
Adrian Curless <awcurless@wpi.edu>
2019-02-24 17:19:26 -0500

    Use bubblwrap's shell demo

--- sandbox_install.sh
+++ sandbox_install.sh
@@ -7,11 +7,13 @@ mkdir -p $SANDBOX_DIRECTORY/${NAMESPACE}/files/var/lib/pacman
 mkdir -p $SANDBOX_DIRECTORY/${NAMESPACE}/files/etc
 cp /etc/pacman.conf $SANDBOX_DIRECTORY/${NAMESPACE}/files/etc/pacman.conf
 
-fakechroot fakeroot pacman -Syu \
+sandboxed_setup() {
+fakechroot fakeroot pacman -Syu --noconfirm \
     --root $SANDBOX_DIRECTORY/${NAMESPACE}/files\
     --dbpath $SANDBOX_DIRECTORY/${NAMESPACE}/files/var/lib/pacman  \
     --config $SANDBOX_DIRECTORY/${NAMESPACE}/files/etc/pacman.conf \
     base fakeroot
+}
 
 sandboxed_install() {
     bwrap \
@@ -24,17 +26,26 @@ sandboxed_install() {
 }
 
 sandboxed_shell() {
-(exec bwrap \
-    --ro-bind $SANDBOX_DIRECTORY/${NAMESPACE}/files / \
-    --ro-bind /etc/resolv.conf /etc/resolv.conf \
-    --tmpfs /tmp \
-    --proc /proc \
-    --dev /dev \
-    --die-with-parent \
-    --unshare-ipc \
-    --unshare-cgroup \
-    --chdir / \
-    sh)
+set -euo pipefail
+(exec bwrap --ro-bind /usr /usr \
+      --dir /tmp \
+      --dir /var \
+      --symlink ../tmp var/tmp \
+      --proc /proc \
+      --dev /dev \
+      --ro-bind /etc/resolv.conf /etc/resolv.conf \
+      --symlink usr/lib /lib \
+      --symlink usr/lib64 /lib64 \
+      --symlink usr/bin /bin \
+      --symlink usr/sbin /sbin \
+      --chdir / \
+      --unshare-all \
+      --unshare-net \
+      --die-with-parent \
+      --dir /run/user/$(id -u) \
+      --setenv XDG_RUNTIME_DIR "/run/user/`id -u`" \
+      --setenv PS1 "${NAMESPACE}> " \
+      /bin/sh) \
 }
 
 # sandboxed_install $2
